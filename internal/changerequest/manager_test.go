203dd25233cacd045063f7655e30de193a498b12
Leland Garofalo <lelandgarofalo@Lelands-MacBook-Pro.local>
2023-08-02 22:52:16 -0700

    test - break build

--- internal/changerequest/manager_test.go
+++ internal/changerequest/manager_test.go
@@ -1,12 +1,13 @@
 package changerequest
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestSubmitChangeRequest(t *testing.T) {
 	_ = New(nil)
-
+	assert.Fail(t, "test")
 	// add mocks
 	// manager.Submit(nil,nil)
 }
