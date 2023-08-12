d4479e8079985778563f01dd35fbc47206e58356
Leland Garofalo <lelandgarofalo@Lelands-MacBook-Pro.local>
2023-08-12 10:56:00 -0700

    fix: adjust path for api for postman run

--- postman/postman.sh
+++ postman/postman.sh
@@ -4,5 +4,7 @@ set -ex
 
 make ci-run
 
-newman run postman/AskDarcel%20API.postman_collection.json -g postman/globals.postman_globals.json
-newman run postman/AskDarcel%20Admin%20API.postman_collection.json -g postman/globals.postman_globals.json
+newman run -x --verbose postman/AskDarcel%20API.postman_collection.json -g postman/globals.postman_globals.json
+newman run -x --verbose postman/AskDarcel%20Admin%20API.postman_collection.json -g postman/globals.postman_globals.json
+
+exit 0
