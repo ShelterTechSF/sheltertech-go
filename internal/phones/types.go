7880a0138c686340ecebe3d68c6d9e9b2e044663
Leland Garofalo <lelandg@gmail.com>
2024-03-06 13:15:34 -0800

    feat: get service by id adjustments (#76)
    Co-authored-by: Leland Garofalo <lelandgarofalo@Lelands-MacBook-Pro.local>

--- internal/phones/types.go
+++ internal/phones/types.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"github.com/nyaruka/phonenumbers"
 	"github.com/sheltertechsf/sheltertech-go/internal/db"
-	"strconv"
 )
 
 type Phone struct {
-	Id          int    `json:"id"`
-	Number      string `json:"number"`
-	ServiceType string `json:"service_type"`
-	Extension   string `json:"extension"`
-	CountryCode string `json:"country_code"`
+	Id          int     `json:"id"`
+	Number      string  `json:"number"`
+	Extension   *string `json:"extension"`
+	ServiceType string  `json:"service_type"`
+	CountryCode string  `json:"country_code"`
 }
 
 func FromDBTypeArray(dbPhones []*db.Phone) []*Phone {
@@ -33,11 +32,9 @@ func FromDBType(dbPhone *db.Phone) *Phone {
 		fmt.Println(err.Error())
 	}
 	if num.NationalNumber != nil {
-		phone.Number = strconv.Itoa(int(*num.NationalNumber))
+		phone.Number = phonenumbers.Format(num, phonenumbers.NATIONAL)
 	}
-	if num.Extension != nil {
-		phone.Extension = *num.Extension
-	}
+	phone.Extension = num.Extension
 	phone.CountryCode = phonenumbers.GetRegionCodeForNumber(num)
 	return phone
 }
