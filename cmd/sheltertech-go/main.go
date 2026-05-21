c89d9519425f6878e1ba24a649e30bded2085347
anchellon <anshulmathew@gmail.com>
2026-05-20 19:56:42 -0700

    The resources change Request handler Migration (#245)
    * The resources change Request handler
    
    * Updated to match generic change request Payload
    
    ---------
    
    Co-authored-by: Richard Xia <richardxia@richardxia.com>

--- cmd/sheltertech-go/main.go
+++ cmd/sheltertech-go/main.go
@@ -166,6 +166,7 @@ func main() {
 
 		r.Delete("/api/phones/{id}", phonesManager.Delete)
 		r.Post("/api/phones/{id}/change_requests", changeRequestManager.UpdatePhone)
+		r.Post("/api/resources/{id}/change_requests", changeRequestManager.UpdateResource)
 		r.Post("/api/change_requests", changeRequestManager.Create)
 	})
 
