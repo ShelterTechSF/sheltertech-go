a38e27410b2e8c78097d7570f0a937b995e622e6
Joe Freund <joe@sheltertech.org>
2026-08-12 19:58:40 -0700

    Add instruction create endpoint

--- internal/db/instructions.go
+++ internal/db/instructions.go
@@ -20,6 +20,12 @@ FROM public.instructions i
 WHERE i.service_id = $1
 `
 
+const createInstructionSql = `
+INSERT INTO public.instructions (service_id, instruction, created_at, updated_at)
+VALUES ($1, $2, NOW(), NOW())
+RETURNING id, instruction
+`
+
 func (m *Manager) GetInstructionsByServiceID(serviceId int) []*Instruction {
 	var rows *sql.Rows
 	var err error
@@ -28,6 +34,18 @@ func (m *Manager) GetInstructionsByServiceID(serviceId int) []*Instruction {
 		log.Printf("%v\n", err)
 	}
 	return scanInstructions(rows)
+}
+
+func (m *Manager) CreateInstruction(serviceId int, instruction string) (*Instruction, error) {
+	row := m.DB.QueryRow(createInstructionSql, serviceId, instruction)
+
+	var created Instruction
+	err := row.Scan(&created.Id, &created.Instruction)
+	if err != nil {
+		return nil, err
+	}
+
+	return &created, nil
 }
 
 func scanInstructions(rows *sql.Rows) []*Instruction {
